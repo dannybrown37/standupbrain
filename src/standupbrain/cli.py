0643e9d6f5b191f613add51677e1b0b2fbb9f135
Danny Brown <dannybrown37@gmail.com>
2025-11-28 08:17:58 -0500

    expand click and ruff stuff

--- src/standupbrain/cli.py
+++ src/standupbrain/cli.py
@@ -1,6 +1,10 @@
+from datetime import datetime
+from pprint import pprint
 import click
 
+from standupbrain.git import get_git_commits
 from standupbrain.llm import init_llm
+from standupbrain.utils import get_previous_workday
 
 
 @click.group()
@@ -11,3 +15,16 @@ def main() -> None:
 @main.command()
 def init() -> None:
     init_llm()
+
+
+@main.command()
+@click.option(
+    '--date',
+    type=click.DateTime(formats=['%Y-%m-%d']),
+    help='Specific date to generate update for (YYYY-MM-DD)',
+)
+def generate(date: datetime | None) -> None:
+    if not date:
+        date = get_previous_workday()
+    commits = get_git_commits(date)
+    pprint(commits)
