da145f589610ec1b8bbe94b8f1091b5eb8ea4519
Danny Brown <dannybrown37@gmail.com>
2025-11-29 08:55:14 -0500

    begin to prepare for pypi, split slow ollama test to e2e, hithub actions shell, et al

--- tests/e2e/llm_test.py
+++ tests/e2e/llm_test.py
@@ -1,9 +1,7 @@
-from conftest import skip_90_percent_for_slower_tests
-
 from standupbrain.llm import prompt_local_llm
 
 
-@skip_90_percent_for_slower_tests
 def test_prompt_local_llm() -> None:
+    """This is obviously not totally deterministic but it passes reliably"""
     result = prompt_local_llm('Just say "Hi!"')
     assert result == 'Hi!'
